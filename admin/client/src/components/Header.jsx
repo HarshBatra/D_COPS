7f89856e195ae781a002495f3d6a6c313c04f364
Sandali Singh <77054645+sandalisingh@users.noreply.github.com>
2022-01-30 01:16:32 +0530

    Update Header.jsx

--- admin/client/src/components/Header.jsx
+++ admin/client/src/components/Header.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 
-// import CloseIcon from '/Applications/Sandali/canteen/node_modules/@mui/icons-material/Close';
-
 function Header(){
     return(
         <header>
             <div className="container">
                 <h3 className="top-bar-title">D_COPS</h3>
                 <i className="top-bar-title fal fa-times"></i>
-                {/* <CloseIcon /> */}
             </div>
         </header>
     );
