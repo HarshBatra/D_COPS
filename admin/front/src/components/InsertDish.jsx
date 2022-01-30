62a01466c183c6b5f803868db3c756a90da67bbe
Sandali Singh <77054645+sandalisingh@users.noreply.github.com>
2022-01-30 21:06:23 +0530

    Update InsertDish.jsx

--- admin/front/src/components/InsertDish.jsx
+++ admin/front/src/components/InsertDish.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import Form from "./Form";
 import Heading from "./Heading";
 
-
-
 function InsertDish(props){
     return(
         <section className="add-dish container">
